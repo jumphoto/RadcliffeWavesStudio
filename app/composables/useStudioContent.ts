6c16342a2087b390eedefa187808a33724c1f4df
jumphoto <jumanahphotography@gmail.com>
2026-05-04 13:51:41 +0200

    import rule newline

--- app/composables/useStudioContent.ts
+++ app/composables/useStudioContent.ts
@@ -1,4 +1,5 @@
 import { artists, navItems, services, studioBrand } from '~/data/studio-content'
+
 export function useStudioContent() {
   const founder = computed(() => artists.find(a => a.role === 'founder'))
   const represented = computed(() => artists.filter(a => a.role === 'represented'))
